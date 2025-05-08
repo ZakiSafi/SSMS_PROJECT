a77baab97a286b2fd0f7e160e815caf94900d1c8
unknown <zakiullahsafi00@gmail.com>
2025-05-08 09:50:19 +0430

    All routes made successfully

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,28 @@
 <?php
 
 use App\Http\Controllers\AcademicYearController;
+use App\Http\Controllers\ClassroomController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\FacultyController;
+use App\Http\Controllers\LogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProvinceController;
+use App\Http\Controllers\RollPermissionController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\StudentStatisticController;
 use App\Http\Controllers\UniversityController;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
 Route::apiResource('provinces', ProvinceController::class);
 Route::apiResource('universities', UniversityController::class);
-Route::apiResource('academic_years', AcademicYearController::class);
+Route::apiResource('academicYears', AcademicYearController::class);
+Route::apiResource('faculties', FacultyController::class);
+Route::apiResource('departments', DepartmentController::class);
+Route::apiResource('classRooms', ClassroomController::class);
+Route::apiResource('studentStatistics', StudentStatisticController::class);
+Route::apiResource('settings', SettingController::class);
+Route::apiResource('rollPermissions', RollPermissionController::class);
+Route::apiResource('users', UserController::class);
+Route::apiResource('logs', LogController::class);
