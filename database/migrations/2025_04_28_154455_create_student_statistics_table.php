a0312906d94f8ac74c420ddd4722e6ea5cdd2199
unknown <zakiullahsafi00@gmail.com>
2025-05-19 21:53:35 +0430

    added new columns to student_statistics and updated the factory and model of it

--- database/migrations/2025_04_28_154455_create_student_statistics_table.php
+++ database/migrations/2025_04_28_154455_create_student_statistics_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->foreignId('faculty_id')->constrained('faculties')->cascadeOnDelete();
             $table->foreignId('department_id')->constrained()->cascadeOnDelete();
             $table->foreignId('classroom_id')->constrained()->cascadeOnDelete();
+            $table->enum('shift', ['day', 'night'])->default('day');
+            $table->enum('season', ['spring', 'autumn', 'summer', 'winter'])->default('spring');
+            $table->tinyInteger('semester_number')->default(1); // 1 for first semester, 2 for second semester
             $table->integer('male_total')->default(0); // Total number of male students
             $table->integer('female_total')->default(0); // Total number of female students
             $table->enum('student_type', ['new', 'current', 'graduated'])->default('new');
